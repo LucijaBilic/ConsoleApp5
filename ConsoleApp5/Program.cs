31190bf60e856d39f952bd1728f9ce80fd9c3bda
student <student@B420-02>
2021-03-25 17:04:20 +0100

    Dodanno

--- ConsoleApp5/Program.cs
+++ ConsoleApp5/Program.cs
@@ -7,6 +7,8 @@ namespace ConsoleApp5
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Novo");
         }
     }
 }
